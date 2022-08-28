fb4cf2b395f3a53911546706b9963132b3bedc4d
Satriansyah Wibowo <satriansyahw@gmail.com>
2022-08-28 10:22:17 +0800

    add Utils : Response

---
+++ BookingCalendar/BookingCalendar/Utils/GenericResponse.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System.Text.Json.Serialization;
+
+namespace BookingCalendar.Utils
+{
+	public class GenericResponse
+	{
+
+        [JsonPropertyOrder(-1)]
+        public int? Status { get; set; } = 200;
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [JsonPropertyOrder(0)]
+        public Boolean? IsSuccess { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        [JsonPropertyOrder(1)]
+        public String? Message { get; set; }
+
+        public GenericResponse() { }
+        public GenericResponse(Boolean? isSuccess, String? message)
+        {
+            this.IsSuccess = isSuccess;
+            this.Message = message;
+        }
+    }
+}
+
